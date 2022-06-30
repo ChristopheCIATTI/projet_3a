39652179f2b55990b9579c1e3be30288fc0f6b9b
ADFDJ\D721585 <christophe.ciatti@hormail.com>
2022-06-30 09:38:04 +0200

    patch

--- projet_3a-back_end/datamodel/basedao.js
+++
@@ -1,6 +0,0 @@
-module.exports = class BaseDAO {
-    constructor(db, tablename) {
-        this.db = db
-        this.tablename = tablename
-    }
-}
--- projet_3a-back_end/services/user.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt')
-const UserDAO = require('../datamodel/userdao')
-const User = require('../datamodel/user')
-
-module.exports = class UserService {
-    constructor(db) {
-        this.dao = new UserDAO(db)
-    }
-}
